eda2f970c2a282d7d57fb6d1f149246fb1cc4607
“Rajeshwari <kotarajeswari003@gmail.com>
2023-07-21 13:36:49 +0530

    [Rajeshwari kota] creating customer table

--- EXAMPLE.sql
+++ EXAMPLE.sql
@@ -1,3 +1,10 @@
 --------------UC1-CREATE DATABASE--------------------------------------------
 create Database EXAMPLE;
 Use Example;
+--------------UC2-CREATE CUSTOMERTABLE----------------------------------------
+CREATE TABLE Customer (
+     ID int primary key identity(1,1),
+	 Name varchar(100),
+	 PhoneNumber Varchar(15),
+	 Address varchar(200)
+);
